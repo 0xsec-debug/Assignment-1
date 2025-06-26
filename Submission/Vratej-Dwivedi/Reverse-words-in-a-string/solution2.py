35fb96ae213164de40b8d8ca693ab120d37b3be1
VRATEJ DWIVEDI <vratejdiwedi000@gmail.com>
2025-06-26 12:19:57 +0530

    Update solution2.py

--- Submission/Vratej-Dwivedi/Reverse-words-in-a-string/solution2.py
+++ Submission/Vratej-Dwivedi/Reverse-words-in-a-string/solution2.py
@@ -1,1 +1,9 @@
-
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()  #Split the string into words        
+        rev_word = words[::-1] #Reverse the list of words
+        return ' '.join(rev_word) #Join the words back into a string with single spaces
