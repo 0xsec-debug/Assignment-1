d969821a803dcbaea3a19e6211a7d21f5ae93021
VRATEJ DWIVEDI <vratejdiwedi000@gmail.com>
2025-06-26 12:13:35 +0530

    Update solution1.py

--- Submission/Vratej-Dwivedi/Twos-sum/solution1.py
+++ Submission/Vratej-Dwivedi/Twos-sum/solution1.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # key: number, value: index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
